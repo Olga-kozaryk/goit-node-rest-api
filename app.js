32a8738bd87761241a7aa6f0a0310b2fbc9b1fec
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-04-21 23:21:23 +0300

    settings DB

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import mongoose from "mongoose";
 
-import contactsRouter from "./routes/contactsRouter.js";
+const DB_URL = "mongodb+srv://Olga:Password_123@cluster0.j0iyrwv.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
 
-const app = express();
-
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/contacts", contactsRouter);
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Database conection succesfull"))
+  .catch((error) => {
+    console.error("Database conection error", error);
+    process.exit(1);
+  });
